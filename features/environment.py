7592e8edaa9bd18cab46391a5cc06ca6c9e62c05
Andres Flores <aafvca@gmail.com>
2018-09-25 16:36:27 -0400

    Fix Firefox driver path

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 def before_all(context):
-    #context.browser = webdriver.Firefox(executable_path="C:\geckodriver.exe")
+    #context.browser = webdriver.Firefox(executable_path="geckodriver.exe")
     context.browser = webdriver.Chrome(executable_path="chromedriver.exe")
 
 def after_all(context):
