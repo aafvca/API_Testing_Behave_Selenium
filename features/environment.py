7040d0752695a1bdbe427557f0fb6f5b5929a42e
Andres Flores <aafvca@gmail.com>
2018-10-01 22:44:22 -0400

    Add Linux support

--- features/environment.py
+++ features/environment.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 def before_all(context):
     #context.browser = webdriver.Firefox(executable_path="geckodriver.exe")
     context.browser = webdriver.Chrome(executable_path="chromedriver.exe")
+    #Linux
+    #context.browser = webdriver.Chrome("/full/path/to/chromedriver")
 
 def after_all(context):
     context.browser.quit()
